12faf0e5f47aacd694d1865c111ef90e6e429e90
raddeuvs <1radeutsch1@gmail.com>
2019-08-04 19:30:55 -0400

    Added first Log.d() statements

--- chapter_002/AppEvents/app/src/main/java/app/actionmobile/appevents/MainActivity.kt
+++ chapter_002/AppEvents/app/src/main/java/app/actionmobile/appevents/MainActivity.kt
@@ -2,11 +2,19 @@ package app.actionmobile.appevents
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Inside OnCreate()...")
+
+        var appEventTag = "MainActivity"
+        var appEventMsg = "Inside OnCreate()... 2"
+
+        Log.d(appEventTag, appEventMsg);
+
     }
 }
